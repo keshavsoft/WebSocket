df2f02ac3ec45ad2bf001b118bdf14cb724ef2b3
Keshav <nknnkumar@yahoo.com>
2023-12-23 10:40:34 +0530

    keshav

---
+++ klowdb.mjs
@@ -0,0 +1,25 @@
+import { JSONPreset } from 'lowdb/node'
+// let JSONPreset = require("lowdb");
+
+
+let StartFunc = async () => {
+
+    // Read or create db.json
+    const defaultData = { posts: [] }
+    const db = await JSONPreset('db.json', defaultData)
+
+    // Create and query items using plain JavaScript
+    db.data.posts.push('hello world')
+    const firstPost = db.data.posts[0]
+
+    // If you don't want to type db.data everytime, you can use destructuring assignment
+    const { posts } = db.data
+    posts.push('hello world')
+
+    // Finally write db.data content to file
+    await db.write()
+
+
+};
+
+StartFunc().then();
